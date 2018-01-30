483bc85e0e91b48d837ce3245a5f950fdf9f7ffe
Drew Smith <drewsmith@mwc-052060.dhcp.missouri.edu>
2018-01-29 21:03:54 -0600

    Second commit

---
+++ Swift Language Basics/Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Drew Smith on 1/29/18.
+//  Copyright © 2018 Tech Innovator. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
